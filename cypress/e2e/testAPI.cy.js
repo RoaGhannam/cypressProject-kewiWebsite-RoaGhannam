f49e527fcd63c16b111c82aa5a5c3f1d040877e2
RoaGhannam <roaghannam3@gmail.com>
2026-05-07 12:50:46 +0300

    New change

--- cypress/e2e/testAPI.cy.js
+++ cypress/e2e/testAPI.cy.js
@@ -4,14 +4,9 @@ describe('API Testing using cy.intercept', () => {
 
   it('Inspect GET products API', () => {
 
-    cy.intercept('GET', '**/products').as('getProducts')
-    cy.visit('https://kewi.ps/products')
-    cy.wait('@getProducts').then((interception) => {
-    cy.log('Status Code: ' + interception.response.statusCode)
-    cy.log('Response Body: ', interception.response.body)
-    expect(interception.response.statusCode).to.eq(200)
+    
 
-    })
+    
 
   })
 
